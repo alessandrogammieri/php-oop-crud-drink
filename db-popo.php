66a129feef265784204637f4746ee172c1f1f77b
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-09-24 22:18:58 +0200

    completed exercise

--- db-popo.php
+++ db-popo.php
@@ -1,3 +1,53 @@
 <?php 
 
+    class Bancone {
+
+        private $id;
+        private $drink_name;
+        private $brand;
+        private $price;
+
+        public function __construct($row) {
+            $this -> setId($row['id']);
+            $this -> setDrink($row['drink_name']);
+            $this -> setBrand($row['brand']);
+            $this -> setPrice($row['price']);
+        }
+
+        public function getId() {
+            return $this -> id;
+        }
+        public function setId($id) {
+            $this -> id = $id;
+        }
+
+        public function getDrink() {
+            return $this -> drink_name;
+        }
+        public function setDrink($drink_name) {
+            $this -> drink_name = $drink_name;
+        }
+
+        public function getBrand() {
+            return $this -> brand;
+        }
+        public function setBrand($brand) {
+            $this -> brand = $brand;
+        }
+
+        public function getPrice() {
+            return $this -> price;
+        }
+        public function setPrice($price) {
+            $this -> price = $price;
+        }
+
+        public function toString() {
+            return $this -> getId() . " " 
+            . $this -> getDrink() . " - " 
+            . $this -> getBrand() . "<br>" 
+            . "Prezzo: € " . $this -> getPrice() . "<br>";
+        }
+    }
+
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,46 @@
     include "db-connection.php";
     include "db-query.php";
 
-    echo "Ciao Mondo";
+    $res = $conn -> query($query_getAllDrinks);
+
+    $drinks = [];
+   
+    if ($res && $res -> num_rows > 0) {
+
+        while ($row = $res -> fetch_assoc()) {
+
+        $drink = new Bancone($row);
+        $drinks[] = $drink;
+        // $drinks[] = new Bancone($row);
+        }
+    }
+
+    for ($i=0; $i<sizeof($drinks); $i++) {
+
+        $drink = $drinks[$i];
+        echo $drink -> toString() . "<br>";
+    }
+    echo "<br>--------------------------------<br><br>";
+    
+    $min = $drinks[0];
+    $max = $drinks[0];
+    $sum = 0;
+
+    for ($i=0; $i<sizeof($drinks); $i++) {
+
+        $drink = $drinks[$i];
+        $sum += $drink -> getPrice();
+
+        if ($min -> getPrice() > $drink -> getPrice()) {
+            $min = $drink;
+        }
+        if ($max -> getPrice() < $drink -> getPrice()) {
+            $max = $drink;
+        }
+    }
+
+    echo "Min: " . $min -> toString() . "<br>";
+    echo "Max: " . $max -> toString() . "<br>";
+    echo "Somma di tutti i prezzi: € " . $sum;
     
 ?>
